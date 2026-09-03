fd7d22fd426812daf1151d692bd09960132fb59f
geetanjalityagi <geetanjalityagi1057@gmail.com>
2026-09-03 15:43:05 +0530

    INitial Commit

---
+++ BankAccount.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class BankAccount{
+    int acc_no, acc_name, balance;
+
+    void deposit(){
+
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new 
+    }
+}
