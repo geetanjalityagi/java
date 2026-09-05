644ab17ec26c23af7fa2705bd9109e986b3d97f5
geetanjalityagi <geetanjalityagi1057@gmail.com>
2026-09-05 18:05:32 +0530

    Initial Commit

--- BankAccount.java
+++ BankAccount.java
@@ -1,13 +1,29 @@
-import java.util.Scanner;
-
 class BankAccount{
-    int acc_no, acc_name, balance;
+    int acc_no;
+    String acc_name;
+    float balance;
 
-    void deposit(){
+    BankAccount(int acc_no,String  acc_name,float balance){
+        this.acc_no = acc_no;
+        this.acc_name = acc_name;
+        this.balance = balance;
+    }
+    void deposit(float amount){
+        this.balance += amount;
+    }
 
+    void withdraw(float amount){
+        this.balance -= amount;
+    }
+
+    void displayBalance(){
+        System.out.println("Balance : " + balance);
     }
 
     public static void main(String[] args){
-        Scanner sc = new 
+        BankAccount obj = new BankAccount(23434, "abc", 6447.0f);
+        obj.deposit(500.0f);
+        obj.withdraw(147.0f);
+        obj.displayBalance();
     }
 }
--- Imply.java
+++ Imply.java
@@ -1,25 +1,3 @@
-// class hello{
-//     int a, b, res;
-
-//     void sum(){
-//         a = 5;
-//         b = 6;
-//         res = a + b;
-
-//         System.out.println("Sum = "+res);
-
-//     }
-// }
-
-// public static void main(String arg[]){
-//     hello obj = new hello();
-//     hello obj2 = new hello();
-
-//     obj.sum();
-//     obj2.sum();
-// }
-
-
 class Imply{
 
     String name;
